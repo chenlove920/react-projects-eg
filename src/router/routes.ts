d8e3f353cf8cab0a25d82b6f13808be9d9310fed
yumo <chenyinxilove@gmail.com>
2025-07-01 14:29:21 +0800

    Revert "react 版本降级"
    This reverts commit 6876949a686ca39f3ecca28010ec253db2afd87e, reversing
    changes made to ecff57e3cc67bbf7c5b1b762943b576d7935f093.

--- src/router/routes.ts
+++
@@ -1,9 +0,0 @@
-import Home from "@/pages/Home";
-import { createBrowserRouter } from "react-router-dom";
-export default  createBrowserRouter([
-    {
-        path:'/',
-        Component:Home,
-    },
-])
-
